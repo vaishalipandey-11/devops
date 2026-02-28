41f238937403e672d90913fe164b49318a855cc6
vaishalipandey-11 <pandeyvaishali002@gmail.com>
2026-02-28 20:26:11 +0530

    this is my first shell script

---
+++ useradd.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#this is a single line comment 
+
+echo " this is my first shell script "
+
+echo " first try to give permission to the file by doing  $ sudo chmod 763 file.sh then execute by / file.sh"
+ 
